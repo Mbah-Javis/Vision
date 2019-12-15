35cecbac0614eb6edc9d20cdccd720c301493762
Mbah-Javis <jelvismbah18@gmail.com>
2019-12-15 06:13:13 +0100

    Initial commit

---
+++ app/src/main/java/com/mbahjavis/vision/MainActivity.java
@@ -0,0 +1,36 @@
+package com.mbahjavis.vision;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Animation animation;
+    Button btnStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                overridePendingTransition(R.anim.anim_toggle, R.anim.anim_toggle);
+                startActivity(intent);
+            }
+        });
+
+      animation = AnimationUtils.loadAnimation(this, R.anim.load2);
+
+      btnStart.startAnimation(animation);
+    }
+}
